939683bd0bf73c33c3319963a4948f53653e32bf
henry <henry@comptune.com>
2010-11-23 08:02:40 +0000

    Added -tabs and -drop options. Fixed more bugs.

--- tests/client.a.c
+++ tests/client.a.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -9,9 +10,9 @@
 /* void error  char *msg
     perror  msg
     exit  1 */
-    
+
 #define error(x) \
-    perror (#x); \
+    perror (x); \
     exit (1);
 
 int main  int argc, char **argv
